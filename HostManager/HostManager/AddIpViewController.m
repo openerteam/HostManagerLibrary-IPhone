1a76bf8651da7223cd829c9281e2c744f82c8ca0
North <yangwork@163.com>
2019-03-21 16:59:39 +0800

    修改

--- HostManager/HostManager/AddIpViewController.m
+++ HostManager/HostManager/AddIpViewController.m
@@ -221,7 +221,6 @@
     
     NSString *ip = [NSString stringWithFormat:@"%@.%@.%@.%@",self.inputTextFiled1.text,self.inputTextFiled2.text,self.inputTextFiled3.text,self.inputTextFiled4.text];
     
-    
     AddressModel *model = [[AddressModel alloc] initWithName:self.nameTextFiled.text.length>0?self.nameTextFiled.text:@"未命名服务器" ip:ip host:self.hostTextFiled.text];
     
     [[HostManagerHelper shareHeler] saveNewItem:model];
